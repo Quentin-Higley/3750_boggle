2fddfc182edf5d56e6b7f8ca8d930c913283de17
Quentin Higley <quentinhigley@Quentins-MacBook-Air.local>
2023-04-05 17:35:28 -0600

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function App() {
+    axios.defaults.withCredentials = true;
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={<Replace />}
+            />
+        </Routes>
+    );
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
