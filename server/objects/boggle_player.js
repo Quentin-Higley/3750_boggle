297278beeca2da74a073e14336f8de0d817c7a82
Quentin Higley <quentinhigley@quentins-air.wireless.weber.edu>
2023-04-10 18:37:53 -0600

    added basic player methods

--- server/objects/boggle_player.js
+++ server/objects/boggle_player.js
@@ -4,6 +4,18 @@ class Player {
         this.score = 0;
         this.words = [];
     }
+
+    add_score(score) {
+        this.score += score;
+    }
+    add_word(word) {
+        this.words.push(word);
+    }
+
+    reset() {
+        this.score = 0;
+        this.words = [];
+    }
 }
 
 export default Player;
